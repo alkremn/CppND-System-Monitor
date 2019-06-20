e2f75aa7a5a83b20621d76e5c9c5d6b75a539fe4
AlexK <alkremn@gmail.com>
2019-06-20 09:50:38 -0600

    Fixed issue with existing id check and formating

--- Process.h
+++ Process.h
@@ -4,7 +4,8 @@
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
-class Process {
+class Process
+{
 private:
     std::string pid;
     std::string user;
@@ -14,7 +15,8 @@ private:
     std::string upTime;
 
 public:
-    Process(std::string pid){
+    Process(std::string pid)
+    {
         this->pid = pid;
         this->user = ProcessParser::getProcUser(pid);
         this->cmd = ProcessParser::getCmd(pid);
@@ -23,49 +25,54 @@ public:
         this->upTime = ProcessParser::getProcUpTime(pid);
     }
     void setPid(int pid);
-    std::string getPid()const;
-    std::string getUser()const;
-    std::string getCmd()const;
-    int getCpu()const;
-    int getMem()const;
-    std::string getUpTime()const;
+    std::string getPid() const;
+    std::string getUser() const;
+    std::string getCmd() const;
+    int getCpu() const;
+    int getMem() const;
+    std::string getUpTime() const;
     std::string getProcess();
 };
-void Process::setPid(int pid){
+void Process::setPid(int pid)
+{
     this->pid = pid;
 }
-std::string Process::getPid()const {
+std::string Process::getPid() const
+{
     return this->pid;
 }
-std::string Process::getProcess(){
+std::string Process::getProcess()
+{
+    if(!ProcessParser::isPidExisting(this-pid)) 
+        return "";
+
     this->mem = ProcessParser::getVmSize(this->pid);
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return this->pid + "   " + this->user + "   "+ this->mem.substr(0,5) + "   " + this->cpu.substr(0, 5) 
-                    + "   " + this->upTime.substr(0, 5) + "   " + this->cmd.substr(0,30) + "...";
+    return this->pid + "   " + this->user + "   " + this->mem.substr(0, 5) + "   " + this->cpu.substr(0, 5) + "   " + this->upTime.substr(0, 5) + "   " + this->cmd.substr(0, 30) + "...";
 }
 
- std::string Process::getUser()const
- {
-     return this->user;
- }
+std::string Process::getUser() const
+{
+    return this->user;
+}
 
-std::string Process::getCmd()const
+std::string Process::getCmd() const
 {
     return this->cmd;
 }
 
-int Process::getCpu()const
+int Process::getCpu() const
 {
     return std::stoi(this->cpu);
 }
-    
-int Process::getMem()const
+
+int Process::getMem() const
 {
     return std::stoi(this->mem);
 }
-std::string Process::getUpTime()const
+std::string Process::getUpTime() const
 {
     return this->upTime;
 }
